6ed8c51ca3bd26bf58c554d82dadbc232aed0236
Leonid Latyshev <forlemonster@gmail.com>
2020-03-21 16:28:04 +1000

    Initial Commit

---
+++ Daily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily
+//
+//  Created by Яна Латышева on 21.03.2020.
+//  Copyright © 2020 Motodolphin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
